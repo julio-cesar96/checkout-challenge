1a695e9c08897066b644b75cb3e11a1aa2262efd
julio-cesar96 <julio_cbs96@hotmail.com>
2025-02-08 10:27:02 -0300

    fix: adjust entry files

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { CheckoutProvider } from "./context/CheckoutContext "
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <CheckoutProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </CheckoutProvider>
+    </ChakraProvider>
+  </React.StrictMode>
+);
